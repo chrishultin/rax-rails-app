ff1f5280d21415079c4e99a908ee19e79fa53d9c
Jason Boyles <jason@duncancreek.net>
2014-07-10 16:38:01 -0500

    break out db setup, rename build_heat_stack

--- recipes/heat_deploy_app.rb
+++ recipes/heat_deploy_app.rb
@@ -1,23 +1,20 @@
 ####
 # fix up attributes that were passed as Heat parameters
 
-# node['railsstack']['rails']['rake_tasks'] comes in as a string,
-# but needs to be a list
-
+# When passed as a parameter from Heat, node['railsstack']['rails']['rake_tasks']
+# comes in as a string of space-separated words, but needs to be a list.
 node.set['railsstack']['rails']['rake_tasks'] = node['railsstack']['rails']['rake_tasks'].split(' ')
 
 ####
 
 if platform_family?('debian')
-  # required for Ruby >= v2
+  # required for Ruby >= v2.0.0
   package 'libssl-dev' do
     action :install
   end
 end
 
 include_recipe "rax-rails-app::app_host_user"
-
-include_recipe "rax-rails-app::#{node['railsstack']['db']['type']}_db_user_setup"
 
 include_recipe "rax-rails-app::ruby_#{node['railsstack']['ruby_manager']}"
 
