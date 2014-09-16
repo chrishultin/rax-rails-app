674265a92b5a6e77d2536d06c8c01e82e44211a7
Jason Boyles <jason@duncancreek.net>
2014-09-16 14:48:12 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'jason.boyles@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures railsstack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends 'apt', '~> 2.4.0'
 depends 'yum'
